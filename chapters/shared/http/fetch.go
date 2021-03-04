0b090cd7b77a046b5f8fa2635ae9851082c02d96
Grady Saccullo <gsaccullo@petrichor.io>
2021-03-03 23:50:11 -0800

    Chapter 1.6 (Fetching URLs Concurrently) nightly checking

---
+++ chapters/shared/http/fetch.go
@@ -0,0 +1,29 @@
+package http
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func Fetch() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, _ = fmt.Fprintf(os.Stdout, "fetch: resp status: %s\n\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		_ = resp.Body.Close()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "fetch: reding %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
